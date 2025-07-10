34f1fba8eeda4634f7670c4596bddfcca9b99d07
Spandan Suthar <117799630+spagdoon0411@users.noreply.github.com>
2025-07-11 04:02:49 +0530

    take file name as arg in socket testing script

--- scripts/test_socket_chain.sh
+++ scripts/test_socket_chain.sh
@@ -1,9 +1,9 @@
 # Upload a sample file and listen over WebSocket
-
 API_ID="<Your stack's API ID>"
 TABLE_NAME="<Your stack's job table name>"
-FILE_NAME="<Sample data to upload>"
 WS_ENDPOINT="<Your stack's WebSocket endpoint>"
+
+FILE_NAME=$1 # File containing the record to upload
 
 python scripts/clear_jobs.py "$TABLE_NAME"
 bash scripts/trigger_api_upload.sh "$API_ID" # Creates upload_out.json in root dir
